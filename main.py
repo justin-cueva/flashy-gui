a6e2d15975880dd48bbe13c0586b58f3a158a10d
Justin Cueva <justincueva@Justins-MBP.lan>
2022-08-18 00:20:20 -0700

    going to the next flashcard

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from ui import AppUI
+import pandas
 
-flash_card_gui = AppUI()
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+flash_card_gui = AppUI(cards_to_learn=to_learn)
--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 import tkinter
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 size_of_canvas = (800, 526)
 
 
 class AppUI:
-    def __init__(self):
+    def __init__(self, cards_to_learn):
+        self.cards_to_learn = cards_to_learn
         self.window = tkinter.Tk()
         self.window.title("FLASHY")
         self.window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -20,14 +22,14 @@ class AppUI:
         self.language = self.canvas.create_text(
             size_of_canvas[0]/2,
             size_of_canvas[1] * 0.30,
-            text="french",
+            text="French",
             font=("Arial", 40, "italic"),
             fill="#000000"
         )
         self.word = self.canvas.create_text(
             size_of_canvas[0]/2,
             size_of_canvas[1]/2,
-            text="Word",
+            text=" ",
             font=("Arial", 60, "bold"),
             fill="#000000"
         )
@@ -37,9 +39,14 @@ class AppUI:
         wrong_button_image = tkinter.PhotoImage(file="images/wrong.png")
         right_button_image = tkinter.PhotoImage(file="images/right.png")
 
-        self.wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0)
+        self.wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0, command=self.next_card)
         self.wrong_button.grid(row=1, column=0)
-        self.right_button = tkinter.Button(image=right_button_image, highlightthickness=0)
+        self.right_button = tkinter.Button(image=right_button_image, highlightthickness=0, command=self.next_card)
         self.right_button.grid(row=1, column=1)
 
+        self.next_card()
         self.window.mainloop()
+
+    def next_card(self):
+        current_card = random.choice(self.cards_to_learn)
+        self.canvas.itemconfig(self.word, text=current_card["French"])
